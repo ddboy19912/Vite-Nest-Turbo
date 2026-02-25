6291daa7903d1df89763c61a09f525d3a69c96ca
Fortune Ogunsusi <fortune@polytope.technology>
2026-02-25 22:37:07 +0100

    fix: 404 on vercel

---
+++ apps/api/src/handler.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import { AppModule } from './app.module';
+
+const expressApp = express();
+const adapter = new ExpressAdapter(expressApp);
+let initialized = false;
+
+async function bootstrap() {
+  if (!initialized) {
+    const app = await NestFactory.create(AppModule, adapter, { logger: false });
+    app.setGlobalPrefix('api');
+    app.enableCors();
+    await app.init();
+    initialized = true;
+  }
+}
+
+export default async (req: any, res: any) => {
+  await bootstrap();
+  expressApp(req, res);
+};
